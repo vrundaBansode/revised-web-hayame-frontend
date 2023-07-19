5155e9c0855f1f9f916f1f158fe3da97497f1cef
Vrunda Bansode <vrunbansode786@gmail.com>
2023-07-19 20:14:39 +0530

    Edit function implemented to the labour details page

---
+++ src/components/DashboradPage/Pages/AllocateLabour/AllocateLabour.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./allocateLabour.css";
+
+const AllocateLabour = () => {
+  return (
+    <div>
+      <h1>AllocateLabour</h1>
+    </div>
+  );
+};
+
+export default AllocateLabour;
