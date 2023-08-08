42adcfbbaa0bded0c14964767c66f5e3a7a53124
Rohan <rohanraut124@gmail.com>
2023-08-08 14:55:30 +0530

    Added Responsive Table

--- src/components/DashboradPage/Pages/AdminWorkforceList/AddWorkforceList.jsx
+++ src/components/DashboradPage/Pages/AdminWorkforceList/AddWorkforceList.jsx
@@ -3,7 +3,10 @@ import { SearchOutlined } from '@ant-design/icons';
 import { Button, Input, Space, Table } from 'antd';
 import Highlighter from 'react-highlight-words';
 import "./addWorkforceList.css"
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { MDBTable, MDBTableBody, MDBTableHead, MDBDataTable } from 'mdbreact';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 
 const AddWorkforceList = () => {
@@ -13,22 +16,22 @@ const AddWorkforceList = () => {
   const searchInput = useRef(null);
 
   const handleSearch = (
-    selectedKeys= "",
-    confirm = (FilterConfirmProps) => {},
+    selectedKeys = "",
+    confirm = (FilterConfirmProps) => { },
     dataIndex,
-    ) => {
-      confirm();
+  ) => {
+    confirm();
     setSearchText(selectedKeys[0]);
     setSearchedColumn(dataIndex);
   };
 
-  const handleReset = (clearFilters = () => {}) => {
+  const handleReset = (clearFilters = () => { }) => {
     clearFilters();
     setSearchText('');
   };
 
   const getColumnSearchProps = (dataIndex) => ({
-    filterDropdown : ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
       <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
         <Input
           ref={searchInput}
@@ -81,16 +84,16 @@ const AddWorkforceList = () => {
     filterIcon: (filtered) => (
       <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
     ),
-    onFilter : (value, record) =>
+    onFilter: (value, record) =>
       record[dataIndex]?.toString().toLowerCase().includes((value).toLowerCase()),
-        onFilterDropdownOpenChange: (visible) => {
-          if (visible) {
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
         setTimeout(() => searchInput.current?.select(), 100);
       }
     },
     render: (text) =>
-    searchedColumn === dataIndex ? (
-      <Highlighter
+      searchedColumn === dataIndex ? (
+        <Highlighter
           highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
           searchWords={[searchText]}
           autoEscape
@@ -99,91 +102,75 @@ const AddWorkforceList = () => {
       ) : (
         text
       ),
-    });
+  });
 
 
 
-    let navigate = useNavigate()
+  let navigate = useNavigate()
 
 
-    const handleEditAction = (e) => {
-      let labourEmail = e.target.parentElement.parentElement.parentElement.cells[2].innerText
-      // console.log(labourEmail)
-      navigate("/dashboard/update-labour-details?email=" + labourEmail)
+  const handleEditAction = (e) => {
+    let labourEmail = e.target.parentElement.parentElement.cells[2].innerText;
+    // console.log(labourEmail)
+    navigate("/dashboard/update-labour-details?email=" + labourEmail)
 
-    }
-    
-    const columns = [
-      {
-      title: 'First Name',
-      dataIndex: 'firstName',
-      key: 'firstName',
-      width: '15%',
-      ...getColumnSearchProps('firstName'),
-    },
-    {
-      title: 'Last Name',
-      dataIndex: 'lastName',
-      key: 'lastName',
-      width: '15%',
-      ...getColumnSearchProps('lastName'),
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-      width: "15%",
-      ...getColumnSearchProps('email'),
-      // sorter: (a, b) => a.address.length - b.address.length,
-      // sortDirections: ['descend', 'ascend'],
-    },
-    {
-      title: 'Skill',
-      dataIndex: 'skill',
-      key: 'skill',
-      width: '10%',
-      ...getColumnSearchProps('skill'),
-    },
-    {
-      title: 'Phone',
-      dataIndex: 'phone',
-      key: 'phone',
-      width: '10%',
-      ...getColumnSearchProps('phone'),
-  },
-  {
-    title: 'Action',
-    dataIndex: 'action',
-    key: 'action',
-    width: '10%',
-    render: (text) => (<div><span onClick={handleEditAction} style={{ marginRight: "1rem", color: "green", cursor: "pointer" }} >{text}</span></div>)
-    // ...getColumnSearchProps('payment'),
   }
-  ];
 
   const [tableData, setTableData] = useState([])
-  
-  useEffect(()=> {
 
-    let d =[]
+  const data = {
+    columns: [
+      {
+        label: 'First Name',
+        field: 'firstName'
+      },
+      {
+        label: 'Last Name',
+        field: 'lastName'
+      },
+      {
+        label: 'Email',
+        field: 'email'
+      },
+      {
+        label: 'Skill',
+        field: 'skill'
+      },
+      {
+        label: 'Phone',
+        field: 'phone'
+      },
+      {
+        label: 'Action',
+        field: 'action',
+      }
+    ],
+
+    rows: tableData
+  }
+
+
+  useEffect(() => {
+
+    let d = []
 
     const fillTable = async () => {
       const response = await fetch('http://45.127.4.151:8000/api/labour-list', {
-      headers: {
-        'Authorization': 'Token '+ JSON.parse(localStorage.getItem("Token")),
-        'Content-Type': 'application/json'
+        headers: {
+          'Authorization': 'Token ' + JSON.parse(localStorage.getItem("Token")),
+          'Content-Type': 'application/json'
         }
       })
       const td = await response.json()
-      for(let i=0; i<td.length; i++){
+      for (let i = 0; i < td.length; i++) {
         d.push({
-          "key": i+1,
+          // "key": i + 1,
           "firstName": td[i]['first_name'],
           "lastName": td[i]['last_name'],
           "email": td[i]['email'],
           "skill": td[i]['skills'],
           "phone": td[i]['phone'],
-          "action": "Edit"
+          "action": <p style={{ cursor: "pointer", textDecoration: "underline", color: "green" }} onClick={handleEditAction}>Edit</p>
         });
       }
       setTableData(d)
@@ -195,7 +182,13 @@ const AddWorkforceList = () => {
 
   return (
     <div className='workforcelist-table-card'>
-      <Table columns={columns} dataSource={tableData} />
+      {/* <Table columns={columns} dataSource={tableData} /> */}
+      <MDBDataTable className="workforcelist-table"
+        responsive
+        striped
+        small
+        bordered
+        data={data} />
     </div>
   )
 }
